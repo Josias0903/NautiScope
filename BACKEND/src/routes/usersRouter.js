a859788f9267ed9aac42c793ba71f138d7171018
Josias0903 <gatoncio67@gmail.com>
2024-10-13 16:12:58 -0300

    volta abdrei :(

--- BACKEND/src/routes/usersRouter.js
+++ BACKEND/src/routes/usersRouter.js
@@ -4,5 +4,38 @@ const { storeUser, loginUser } = require('../controller/usersController');
 router.post('/register', storeUser);
 router.post('/login', loginUser); // Nova rota para login
 
+/**
+ * @swagger
+ * /api/loginUser:
+ *   get:
+ *     summary: Consulta o usuário
+ *     responses:
+ *        200:
+ *           description: Faz a consulta de todos os dados do usuário pelo id
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: array
+ *                 items:
+ *                   type: object
+ */
+router.get('/login/:id', selecionarUsuario);
+
+/**
+ * @swagger
+ * /api/storeUser:
+ *   put:
+ *     summary: Atualiza os dados
+ *     responses:
+ *        200:
+ *           description: Atualiza os dados do usuário
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: array
+ *                 items:
+ *                   type: object
+ */
+router.put('/register', atualizarDados);
 
 module.exports = router;
--- BACKEND/src/server.js
+++ BACKEND/src/server.js
@@ -3,3 +3,24 @@ const app = require('./app');
 const PORT = app.get('port');
 
 app.listen(PORT, () => console.log(`Running at port ${PORT}`));
+
+const swaggerUi = require("swagger-ui-express")
+const swaggerJsDoc = require("swagger-jsdoc")
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Nautiscope",
+            version: "1.0.0",
+            description: "API CRUD para gerenciar rotas",
+        },
+        servers: [{ url: "http://localhost:3005"}]
+    },
+    apis: [`${__dirname}/routes/*.js`],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+
+app.listen(port, () => console.log(`Run on port ${port}!`));
